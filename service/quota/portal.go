92f53b31aad4966aeee3e3c2f6af5daf872ccb09
Ruiyang Guan <capgrey@linux.do>
2026-05-03 21:20:33 +0800

    feat(quota): send SIGHUP after save/delete to reload config

--- service/quota/portal.go
+++ service/quota/portal.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/sagernet/sing-box/adapter"
@@ -173,6 +174,7 @@ func (h *portalOutbound) handleSaveShadowsocksOutbound(conn net.Conn, req *http.
 		h.writeHTML(conn, http.StatusBadRequest, buildPage("Bad request", fmt.Sprintf(`<div class="card">Invalid Shadowsocks outbound: %s</div>`, html.EscapeString(err.Error()))), nil)
 		return
 	}
+	_ = syscall.Kill(os.Getpid(), syscall.SIGHUP)
 	h.writeHTML(conn, http.StatusSeeOther, "", map[string]string{"Location": "/"})
 }
 
@@ -220,6 +222,7 @@ func (h *portalOutbound) handleDeleteMemberOutbound(conn net.Conn, inboundTag st
 		h.writeHTML(conn, http.StatusBadRequest, buildPage("Bad request", fmt.Sprintf(`<div class="card">Delete custom outbound failed: %s</div>`, html.EscapeString(err.Error()))), nil)
 		return
 	}
+	_ = syscall.Kill(os.Getpid(), syscall.SIGHUP)
 	h.writeHTML(conn, http.StatusSeeOther, "", map[string]string{"Location": "/"})
 }
 
